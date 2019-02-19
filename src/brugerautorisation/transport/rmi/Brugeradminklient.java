331840e52257970561a35806007e6c6b56ec74fe
Unknown <christian.s.mark@hotmail.com>
2019-02-19 22:42:06 +0100

    Fjernelse af ekstra felter
    Tilføjet metoden "fjernAlleEkstraFelt(String brugernavn, String adgangskode):void", så brugerne kan fjerne evt. felter der giver XMLParser fejl.

--- src/brugerautorisation/transport/rmi/Brugeradminklient.java
+++ src/brugerautorisation/transport/rmi/Brugeradminklient.java
@@ -5,26 +5,35 @@ import brugerautorisation.data.Bruger;
 import java.rmi.Naming;
 
 public class Brugeradminklient {
-	public static void main(String[] arg) throws Exception {
-//		Brugeradmin ba =(Brugeradmin) Naming.lookup("rmi://localhost/brugeradmin");
-		Brugeradmin ba = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
 
-		Bruger off = ba.hentBrugerOffentligt("s123456");
-		System.out.println("Fik offentlige data " + Diverse.toString(off));
+    public static void main(String[] arg) throws Exception {
+	Brugeradmin ba =(Brugeradmin) Naming.lookup("rmi://localhost/brugeradmin");
+        //Brugeradmin ba = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
 
-    //ba.sendGlemtAdgangskodeEmail("s123456", "Dette er en test, husk at skifte kode");
-		//ba.ændrAdgangskode("s123456", "kode1xyz", "kode1xyz");
-		Bruger b = ba.hentBruger("s123456", "kode1xyz");
-		System.out.println("Fik bruger " + b);
-		System.out.println("med data " + Diverse.toString(b));
-		// ba.sendEmail("jacno", "xxx", "Hurra det virker!", "Jeg er så glad");
+        Bruger off = ba.hentBrugerOffentligt("s123456");
+        System.out.println("Fik offentlige data " + Diverse.toString(off));
 
-		Object ekstraFelt = ba.getEkstraFelt("s123456", "kode1xyz", "hobby");
-		System.out.println("Brugerens hobby er: " + ekstraFelt);
+        //ba.sendGlemtAdgangskodeEmail("s123456", "Dette er en test, husk at skifte kode");
+        //ba.ændrAdgangskode("s123456", "kode1xyz", "kode1xyz");
+        Bruger b = ba.hentBruger("s123456", "kode1xyz");
+        System.out.println("Fik bruger " + b);
+        System.out.println("med data " + Diverse.toString(b));
+        // ba.sendEmail("jacno", "xxx", "Hurra det virker!", "Jeg er så glad");
 
-		ba.setEkstraFelt("s123456", "kode1xyz", "hobby", "Tennis og programmering"); // Skriv noget andet her
+        Object ekstraFelt = ba.getEkstraFelt("s123456", "kode1xyz", "hobby");
+        System.out.println("Brugerens hobby er: " + ekstraFelt);
 
-		String webside = (String) ba.getEkstraFelt("s123456", "kode1xyz", "webside");
-		System.out.println("Brugerens webside er: " + webside);
-	}
+        ba.setEkstraFelt("s123456", "kode1xyz", "hobby", "Tennis og programmering"); // Skriv noget andet her
+
+        String webside = (String) ba.getEkstraFelt("s123456", "kode1xyz", "webside");
+        System.out.println("Brugerens webside er: " + webside);
+
+        //Test af fjern felter
+        ba.fjernAlleEkstraFelt("s123456", "kode1xyz");
+        b = ba.hentBruger("s123456", "kode1xyz");
+        System.out.println("Fik bruger " + b);
+        System.out.println("med data " + Diverse.toString(b));
+        ekstraFelt = ba.getEkstraFelt("s123456", "kode1xyz", "hobby");
+        System.out.println("Brugerens hobby er: " + ekstraFelt);
+    }
 }
